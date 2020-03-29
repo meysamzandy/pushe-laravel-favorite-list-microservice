7025b740380da8fbee384fdccf86cfdf317c589e
meysamzandy <zandy.meysam@gmail.com>
2020-03-29 17:03:06 +0430

    feat (v1)(MVC) create controller migration model factory and seeder

---
+++ database/migrations/2020_03_29_121321_create_watch_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('watch_lists', static function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned()->autoIncrement()->unique();
+            $table->uuid('uuid')->comment('store uuid of users in ghatreh');
+            $table->integer('nid')->unsigned()->comment('store nid of products');
+            $table->timestamps();
+            $table->index('id');
+            $table->index('uuid');
+            $table->index('nid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('watch_lists');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\WatchList::class,10)->create();
+
     }
 }

85d10deb02edc785a1f7b59da3343fc3ecaae6b1
meysamzandy <zandy.meysam@gmail.com>
2020-03-29 17:46:25 +0430

    feat (v1)(Helper) create WatchListValidators Helper

---
+++ app/Http/Helper/WatchListValidators.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Http\Helper;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class WatchListValidators
+{
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public static function validatorInReActions(request $request): bool
+    {
+        $validator = Validator::make($request->all(),
+            ['n' => 'required|numeric|min:1', 'u' => 'required|min:64']
+        );
+        return !$validator->fails();
+    }
+
+    /**
+     * @param $uuid
+     * @return bool
+     */
+    public static function uuidValidator($uuid): bool
+    {
+        $validator = Validator::make($uuid,
+            ['uuid' => 'uuid']
+        );
+        return !$validator->fails();
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public static function secretValidator(request $request): bool
+    {
+        $validator = Validator::make($request->all(),
+            ['u' => 'required|min:64']
+        );
+        return !$validator->fails();
+    }
+
+}

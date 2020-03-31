2a38278be1aa12b7525d8eea98dfe4376fef4016
meysamzandy <zandy.meysam@gmail.com>
2020-04-01 00:25:13 +0430

    feat (v1)(addToWatchList) create add To Watch List api

--- app/WatchList.php
+++ app/WatchList.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed nid
+ * @property string uuid
+ */
 class WatchList extends Model
 {
-    //
+    protected $fillable = ['nid','uuid'];
 }
